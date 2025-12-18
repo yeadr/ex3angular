38c873c3a83f28d058b842f9ccb7ccc8b90a321a
yeadr <yussef.elalami@institutvidreres.cat>
2025-12-18 13:44:49 +0100

    Ex 5

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 import { Ex1 } from './ex1/ex1';
 import { Ex2 } from './ex2/ex2';
 import { Ex3 } from './ex3/ex3';
+import { Ex4 } from './ex4/ex4';
+import { Ex5 } from './ex5/ex5';
 
 @Component({
   selector: 'app-root',
-  imports: [Ex1, Ex2, Ex3],
+  imports: [Ex1, Ex2, Ex3, Ex4, Ex5],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
